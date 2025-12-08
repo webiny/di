d6bd7e337467c4558cc6540175512aab83c2cc9f
Pavel Denisjuk <pavel@webiny.com>
2025-12-08 17:43:12 +0100

    chore: switch from tsup to rslib

---
+++ rslib.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "@rslib/core";
+
+export default defineConfig({
+  lib: [{ format: "esm", syntax: "esnext", dts: true, bundle: true }]
+});
--- tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  target: "esnext",
-  entry: ["src/index.ts"],
-  sourcemap: true,
-  clean: true
-});
