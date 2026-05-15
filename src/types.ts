c563dc2ef106ae4978702794a2e3da2243fa8833
Bruno Zorić <bruno.zoric@gmail.com>
2026-05-15 17:33:27 +0200

    chore: update dependencies (#9)

--- src/types.ts
+++ src/types.ts
@@ -60,7 +60,7 @@ export type MapDependencies<T extends [...any]> = {
       ? // Support shorthand and long form.
         [Abstraction<T[K]>, OptionalTrue & Partial<MultipleFalse>]
       : // Support shorthand and long form.
-        | [Abstraction<T[K]>, MultipleFalse & Partial<OptionalFalse>]
+          | [Abstraction<T[K]>, MultipleFalse & Partial<OptionalFalse>]
           | [Abstraction<T[K]>]
           | Abstraction<T[K]>;
 };
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   resolve: {
     alias: [{ find: /^~(.*)/, replacement: `${process.cwd()}/src$1` }]
+  },
+  test: {
+    globals: true,
+    include: ["__tests__/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["src/*.ts"],
+      exclude: ["**/index.ts", "**/abstractions/**", "**/feature.ts", "src/DependencyGraph.ts"]
+    }
   }
 });
